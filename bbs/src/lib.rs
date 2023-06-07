c118e163b79a17e208342b53a9b157a7f6c53e63
Ji Park <ji@jipark.dev>
2023-06-07 00:44:49 -0700

    Start looking into testing.

--- bbs/src/lib.rs
+++ bbs/src/lib.rs
@@ -38,3 +38,71 @@ fn load_env() {
     let env_file_path = format!("{}/.env", current_crate_dir);
     dotenvy::from_filename(env_file_path).ok();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::http::StatusCode;
+    use axum::testing::prelude::*;
+    use axum::{routing::get, Router};
+
+    use crate::models::*;
+
+    // Helper function to create a test board
+    fn create_test_board(conn: &PgConnection) -> Board {
+        // Insert a test board into the database
+        let new_board = models::NewBoard {
+            name: todo!(),
+            user_id: todo!(),
+            created_at: todo!(),
+            modified_at: todo!(),
+            user_ip: todo!(),
+        };
+
+        diesel::insert_into(schema::boards::table)
+            .values(&new_board)
+            .get_result(conn)
+            .expect("Failed to create test board")
+    }
+
+    #[tokio::test]
+    async fn test_get_board_by_id() {
+        // Set up the test environment
+        load_env();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+        let conn =
+            PgConnection::establish(&database_url).expect("Failed to connect to the database");
+
+        // Create a test board
+        let test_board = create_test_board(&conn);
+
+        // Create the app with the test board as an extension
+        let app = Router::new()
+            .route(
+                "/boards/:id",
+                get(
+                    extract::Extension::<PgConnection>::from_shared(conn.clone())
+                        .and_then(get_board_by_id),
+                ),
+            )
+            .layer(AddExtensionLayer::new(conn));
+
+        // Send a request to get the test board by ID
+        let response = request::get("/boards/1")
+            .header("Content-Type", "application/json")
+            .reply(&app)
+            .await;
+
+        // Assert that the response is successful
+        assert_eq!(response.status(), StatusCode::OK);
+
+        // Extract the response body
+        let body = extract::json::<Value>(response.into_response().into_body())
+            .await
+            .unwrap();
+
+        // Assert the expected response body based on the test board
+        assert_eq!(body["thread"]["id"], test_board.id);
+        // ... Assert other properties of the test board
+    }
+}
--- bbs/src/models.rs
+++ bbs/src/models.rs
@@ -7,57 +7,57 @@ use serde::{Deserialize, Serialize};
 #[diesel(table_name=boards)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Board {
-    id: i32,
-    name: String,
-    user_id: i32,
-    created_at: NaiveDateTime,
-    modified_at: NaiveDateTime,
-    user_ip: String,
+    pub id: i32,
+    pub name: String,
+    pub user_id: i32,
+    pub created_at: NaiveDateTime,
+    pub modified_at: NaiveDateTime,
+    pub user_ip: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Queryable, Selectable)]
 #[diesel(table_name=posts)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Post {
-    id: i32,
-    thread_id: i32,
-    user_id: i32,
-    content: String,
-    created_at: NaiveDateTime,
-    modified_at: NaiveDateTime,
-    user_ip: String,
+    pub id: i32,
+    pub thread_id: i32,
+    pub user_id: i32,
+    pub content: String,
+    pub created_at: NaiveDateTime,
+    pub modified_at: NaiveDateTime,
+    pub user_ip: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Queryable, Selectable)]
 #[diesel(table_name=comments)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Comment {
-    id: i32,
-    post_id: i32,
-    user_id: i32,
-    content: String,
-    created_at: NaiveDateTime,
-    modified_at: NaiveDateTime,
-    user_ip: String,
+    pub id: i32,
+    pub post_id: i32,
+    pub user_id: i32,
+    pub content: String,
+    pub created_at: NaiveDateTime,
+    pub modified_at: NaiveDateTime,
+    pub user_ip: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Queryable, Selectable)]
 #[diesel(table_name=replies)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Reply {
-    id: i32,
-    comment_id: i32,
-    parent_reply_id: Option<i32>,
-    user_id: i32,
-    content: String,
-    created_at: NaiveDateTime,
-    modified_at: NaiveDateTime,
-    user_ip: String,
+    pub id: i32,
+    pub comment_id: i32,
+    pub parent_reply_id: Option<i32>,
+    pub user_id: i32,
+    pub content: String,
+    pub created_at: NaiveDateTime,
+    pub modified_at: NaiveDateTime,
+    pub user_ip: String,
 }
 
 #[derive(Insertable)]
 #[diesel(table_name=boards)]
-pub struct NewThread<'a> {
+pub struct NewBoard<'a> {
     pub name: &'a str,
     pub user_id: i32,
     pub created_at: NaiveDateTime,
