950adc562bd9f8dc3ddf7249070e10f370d921f3
Ji Park <ji@jipark.dev>
2023-06-11 13:02:31 -0700

    Fix init_db.sh DB_PORT -> DB_HOST env.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Multiplex
+
+[![General](https://github.com/jpnws/multiplex/actions/workflows/general.yml/badge.svg)](https://github.com/jpnws/multiplex/actions/workflows/general.yml)
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -21,7 +21,7 @@ DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
-DB_PORT="${POSTGRES_HOST:=localhost}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]; then
