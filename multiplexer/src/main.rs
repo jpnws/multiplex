84e19bde49c98ae5a1112046b7a4e31f4222ccc2
Ji Park <ji@jipark.dev>
2023-06-13 22:28:06 -0700

    Add bunyan tracing.

--- multiplexer/src/main.rs
+++ multiplexer/src/main.rs
@@ -1,11 +1,19 @@
-use env_logger::Env;
 use multiplexer::cfg::AppSettings;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("multiplex".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber.");
     let cfg = AppSettings::new().expect("Failed to read configuration.");
     let newsletter_db_settings =
         newsletter::cfg::DatabaseSettings::new().expect("Failed to get config.");
