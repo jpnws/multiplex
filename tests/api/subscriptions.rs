eee96c0af8df2abb9ecb74f05962c98f40e9911b
Ji Park <ji@jipark.dev>
2023-06-19 09:57:16 -0700

    Add a helper method that takes the body payload of subscription post and responds with reqwest::Response.

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -7,18 +7,11 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     // ====================================
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
     let body = "name=penguin&email=penguin%40gmail.com";
     // ====================================
     // Act
     // ====================================
-    let response = client
-        .post(&format!("{}/subscriptions", &app.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
+    let response = app.post_subscriptions(body.into()).await;
     // ====================================
     // Assert
     // ====================================
@@ -39,24 +32,17 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 #[case("", "missing both name and email")]
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing(
-    #[case] invalid_body: String,
-    #[case] error_message: String,
+    #[case] body: String,
+    #[case] err_msg: String,
 ) {
     // ====================================
     // Arrange
     // ====================================
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
     // ====================================
     // Act
     // ====================================
-    let response = client
-        .post(&format!("{}/subscriptions", &app.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(invalid_body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
+    let response = app.post_subscriptions(body).await;
     // ====================================
     // Assert
     // ====================================
@@ -65,7 +51,7 @@ async fn subscribe_returns_a_400_when_data_is_missing(
         response.status().as_u16(),
         // Additional customised error message on test failure
         "The API did not fail with 400 Bad Request when the payload was {}.",
-        error_message
+        err_msg
     );
 }
 
@@ -76,23 +62,16 @@ async fn subscribe_returns_a_400_when_data_is_missing(
 #[tokio::test]
 async fn subscribe_returns_a_400_when_fields_are_present_but_invalid(
     #[case] body: String,
-    #[case] error_message: String,
+    #[case] err_msg: String,
 ) {
     // ====================================
     // Arrange
     // ====================================
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
     // ====================================
     // Act
     // ====================================
-    let response = client
-        .post(&format!("{}/subscriptions", &app.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
+    let response = app.post_subscriptions(body).await;
     // ====================================
     // Assert
     // ====================================
@@ -100,6 +79,6 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid(
         400,
         response.status().as_u16(),
         "The API did not return a 400 Bad Request when the payload was {}",
-        error_message
+        err_msg
     );
 }
