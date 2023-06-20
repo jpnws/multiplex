96a25240ca3dea00c44a31c347e000882d92eca4
Ji Park <ji@jipark.dev>
2023-06-19 22:01:11 -0700

    Refactor. Create a separate route handler for subscription registration confirmation email.

---
+++ src/routes/subscriptions_confirm.rs
@@ -0,0 +1,14 @@
+use actix_web::{web, HttpResponse};
+
+#[derive(serde::Deserialize)]
+pub struct Parameters {
+    subscription_token: String,
+}
+
+// Adding the query parameters for `confirm` instruct actix-web to only call
+// the this handler if the extraction of the parameter value was successful.
+// If the extraction fails, a 400 Bad Request is returned to the caller.
+#[tracing::instrument(name = "Confirm a pending subscriber", skip(_parameters))]
+pub async fn confirm(_parameters: web::Query<Parameters>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
