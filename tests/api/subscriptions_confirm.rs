941a34e37bf72659603562cfa909a4c0b81fb28f
Ji Park <ji@jipark.dev>
2023-06-20 22:35:37 -0700

    Add test case to check if clicking on email confirmation link confirms a subscriber.

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -38,3 +38,37 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
     // Assert
     assert_eq!(400, response.status().as_u16());
 }
+
+#[tokio::test]
+async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=penguin&email=penguin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(email_request);
+
+    // Act
+    reqwest::get(confirmation_links.html)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!("penguin@gmail.com", saved.email);
+    assert_eq!("penguin", saved.name);
+    assert_eq!("confirmed", saved.status);
+}
