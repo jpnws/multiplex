cf9ae2fa743eee0db5619320f181432d5b7a3cd4
Ji Park <ji@jipark.dev>
2023-06-22 22:30:44 -0700

    Add newsletters route.

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,16 @@
-use crate::configuration::DatabaseSettings;
-use crate::configuration::Settings;
-use crate::email_client::EmailClient;
-use crate::routes::confirm;
-use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+
+use crate::configuration::DatabaseSettings;
+use crate::configuration::Settings;
+use crate::email_client::EmailClient;
+use crate::routes::confirm;
+use crate::routes::publish_newsletter;
+use crate::routes::{health_check, subscribe};
 
 pub struct Application {
     port: u16,
@@ -25,7 +26,9 @@ impl Application {
             .email_client
             .sender()
             .expect("Invalid sender email address.");
+
         let timeout = configuration.email_client.timeout();
+
         let email_client = EmailClient::new(
             configuration.email_client.base_url,
             sender_email,
@@ -37,8 +40,11 @@ impl Application {
             "{}:{}",
             configuration.application.host, configuration.application.port
         );
+
         let listener = TcpListener::bind(address)?;
+
         let port = listener.local_addr().unwrap().port();
+
         let server = run(
             listener,
             connection_pool,
@@ -78,20 +84,24 @@ fn run(
     base_url: String,
 ) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
+
     let email_client = web::Data::new(email_client);
+
     let base_url = web::Data::new(ApplicationBaseUrl(base_url));
+
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
-            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
+            .route("/newsletters", web::post().to(publish_newsletter))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
     })
     .listen(listener)?
     .run();
+
     Ok(server)
 }
