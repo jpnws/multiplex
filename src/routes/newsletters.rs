f1b84ce385e0912152cb88d979cf6d4ea2d4dad4
Ji Park <>
2023-06-23 11:06:07 -0700

    Add functions to further handle the process of confirming a user and publishing a newsletter, as well as implementing error handling.

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,4 +1,13 @@
+use actix_web::http::StatusCode;
+use actix_web::ResponseError;
 use actix_web::{web, HttpResponse};
+use sqlx::PgPool;
+
+use crate::routes::error_chain_fmt;
+
+struct ConfirmedSubscriber {
+    email: String,
+}
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -12,8 +21,53 @@ pub struct Content {
     text: String,
 }
 
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
 // We are prefixing `body` with a `_` to avoid a compiler warning about unused
 // arguments.
-pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn publish_newsletter(
+    pool: web::Data<PgPool>,
+    _body: web::Json<BodyData>,
+) -> Result<HttpResponse, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&pool).await?;
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+            SELECT
+                email
+            FROM
+                subscriptions
+            WHERE
+                status = 'confirmed'
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+
+    Ok(rows)
 }
