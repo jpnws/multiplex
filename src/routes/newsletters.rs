4e71a252b2a9e291e38275bf78194ede58b662db
Ji Park <>
2023-06-23 13:37:36 -0700

    Finish the implementation for publish_newsletter function and refactor get_confirmed_subscribers function accordingly.

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,12 +1,15 @@
 use actix_web::http::StatusCode;
 use actix_web::ResponseError;
 use actix_web::{web, HttpResponse};
+use anyhow::Context;
 use sqlx::PgPool;
 
+use crate::domain::SubscriberEmail;
+use crate::email_client::EmailClient;
 use crate::routes::error_chain_fmt;
 
 struct ConfirmedSubscriber {
-    email: String,
+    email: SubscriberEmail,
 }
 
 #[derive(serde::Deserialize)]
@@ -45,18 +48,53 @@ impl ResponseError for PublishError {
 // arguments.
 pub async fn publish_newsletter(
     pool: web::Data<PgPool>,
-    _body: web::Json<BodyData>,
+    body: web::Json<BodyData>,
+    email_client: web::Data<EmailClient>,
 ) -> Result<HttpResponse, PublishError> {
-    let _subscribers = get_confirmed_subscribers(&pool).await?;
+    let subscribers = get_confirmed_subscribers(&pool).await?;
+
+    for subscriber in subscribers {
+        match subscriber {
+            Ok(subscriber) => {
+                email_client
+                    .send_email(
+                        &subscriber.email,
+                        &body.title,
+                        &body.content.html,
+                        &body.content.text,
+                    )
+                    .await
+                    .with_context(|| {
+                        format!("Failed to send newsletter issue to {}", subscriber.email)
+                    })?;
+            }
+            Err(error) => {
+                tracing::warn!(
+                    // We record the error chain as a structured field on the log
+                    // record.
+                    error.cause_chain = ?error,
+                    // Using `\` to split a long string literal over two lines,
+                    // without creating a `\n` character.
+                    "Skipping a confirmed subscriber. \
+                    Their stored contact details are invalid",
+                );
+            }
+        };
+    }
+
     Ok(HttpResponse::Ok().finish())
 }
 
 #[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
 async fn get_confirmed_subscribers(
     pool: &PgPool,
-) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
-    let rows = sqlx::query_as!(
-        ConfirmedSubscriber,
+    // We are returning a `Vec` of `Result`s in the happy case. This allows the
+    // caller to bubble up errors due to network issues or other transient
+    // failures using the `?` operator, while the compiler forces them to handle
+    // the subtle mapping error. See http://sled.rs/errors.html for a deep-dive
+    // about this technique.
+) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
+    let confirmed_subscribers = sqlx::query!(
         r#"
             SELECT
                 email
@@ -67,7 +105,14 @@ async fn get_confirmed_subscribers(
         "#,
     )
     .fetch_all(pool)
-    .await?;
+    .await?
+    // Map into the domain type.
+    .into_iter()
+    .map(|r| match SubscriberEmail::parse(r.email) {
+        Ok(email) => Ok(ConfirmedSubscriber { email }),
+        Err(error) => Err(anyhow::anyhow!(error)),
+    })
+    .collect();
 
-    Ok(rows)
+    Ok(confirmed_subscribers)
 }
