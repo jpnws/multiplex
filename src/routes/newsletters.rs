9bbb7e76ddbf06a18ef9bcf509fdd8c4d6fffab0
Ji Park <ji@jipark.dev>
2023-06-24 07:45:52 -0700

    Add a function to validate credentials, retrieving user_id from username and password. Afterward, `cargo sqlx prepare` since we're adding a new query.

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -192,3 +192,31 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
         password: Secret::new(password),
     })
 }
+
+async fn validate_credentials(
+    credentials: Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let user_id: Option<_> = sqlx::query!(
+        r#"
+        SELECT
+            user_id
+        FROM
+            users
+        WHERE
+            username = $1
+            AND password = $2
+        "#,
+        credentials.username,
+        credentials.password.expose_secret()
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to perform a query to validate auth credentials.")
+    .map_err(PublishError::UnexpectedError)?;
+
+    user_id
+        .map(|row| row.user_id)
+        .ok_or_else(|| anyhow::anyhow!("Invalid username or password."))
+        .map_err(PublishError::AuthError)
+}
