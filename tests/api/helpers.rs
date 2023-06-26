5f094f3b94b01d2002064a89106d61c8443a2885
Ji Park <>
2023-06-26 15:53:42 -0700

    Implement cookies for login failure message. Create integration tests accordingly.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -41,6 +41,21 @@ pub struct ConfirmationLinks {
 }
 
 impl TestApp {
+    pub async fn post_login<Body>(&self, body: &Body) -> reqwest::Response
+    where
+        Body: serde::Serialize,
+    {
+        reqwest::Client::builder()
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .unwrap()
+            .post(&format!("{}/login", &self.address))
+            .form(body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
         reqwest::Client::new()
             .post(&format!("{}/subscriptions", &self.address))
@@ -183,4 +198,9 @@ impl TestUser {
         .await
         .expect("Failed to store test user.");
     }
+}
+
+pub fn assert_is_redirect_to(response: &reqwest::Response, location: &str) {
+    assert_eq!(303, response.status().as_u16());
+    assert_eq!(location, response.headers().get("Location").unwrap());
 }
