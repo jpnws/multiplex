8b5cbdb7e327ff8fc74c4d7457d3ceb45d01c82a
Ji Park <>
2023-06-29 11:30:13 -0700

    Sync startup.rs with zero2prod repository.

--- src/startup.rs
+++ src/startup.rs
@@ -1,26 +1,22 @@
+use crate::configuration::{DatabaseSettings, Settings};
+use crate::email_client::EmailClient;
+use crate::routes::{
+    admin_dashboard, change_password, change_password_form, confirm, health_check, home, log_out,
+    login, login_form, publish_newsletter, subscribe,
+};
 use actix_session::storage::RedisSessionStore;
 use actix_session::SessionMiddleware;
 use actix_web::cookie::Key;
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use actix_web_flash_messages::storage::CookieMessageStore;
 use actix_web_flash_messages::FlashMessagesFramework;
-use secrecy::ExposeSecret;
-use secrecy::Secret;
+use secrecy::{ExposeSecret, Secret};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
-
-use crate::configuration::DatabaseSettings;
-use crate::configuration::Settings;
-use crate::email_client::EmailClient;
-use crate::routes::publish_newsletter;
-use crate::routes::{admin_dashboard, log_out};
-use crate::routes::{change_password, change_password_form};
-use crate::routes::{confirm, login_form};
-use crate::routes::{health_check, subscribe};
-use crate::routes::{home, login};
 
 pub struct Application {
     port: u16,
@@ -28,7 +24,6 @@ pub struct Application {
 }
 
 impl Application {
-    // A hook that builds and returns server with configuration.
     pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
         let connection_pool = get_connection_pool(&configuration.database)
             .await
@@ -38,9 +33,7 @@ impl Application {
             .email_client
             .sender()
             .expect("Invalid sender email address.");
-
         let timeout = configuration.email_client.timeout();
-
         let email_client = EmailClient::new(
             configuration.email_client.base_url,
             sender_email,
@@ -52,11 +45,8 @@ impl Application {
             "{}:{}",
             configuration.application.host, configuration.application.port
         );
-
         let listener = TcpListener::bind(address)?;
-
         let port = listener.local_addr().unwrap().port();
-
         let server = run(
             listener,
             connection_pool,
@@ -74,8 +64,6 @@ impl Application {
         self.port
     }
 
-    // A more expressive name that makes it clear that this function only
-    // returns when the application is stopped.
     pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
         self.server.await
     }
@@ -88,9 +76,6 @@ pub async fn get_connection_pool(configuration: &DatabaseSettings) -> Result<PgP
         .await
 }
 
-// Define a wrapper type to retrieve the URL in `subscribe` handler. Retrieval
-// from the context in actix-web is type-based: using a raw `String` would
-// expose us to conflicts.
 pub struct ApplicationBaseUrl(pub String);
 
 async fn run(
@@ -101,20 +86,13 @@ async fn run(
     hmac_secret: Secret<String>,
     redis_uri: Secret<String>,
 ) -> Result<Server, anyhow::Error> {
-    let db_pool = web::Data::new(db_pool);
-
-    let email_client = web::Data::new(email_client);
-
-    let base_url = web::Data::new(ApplicationBaseUrl(base_url));
-
+    let db_pool = Data::new(db_pool);
+    let email_client = Data::new(email_client);
+    let base_url = Data::new(ApplicationBaseUrl(base_url));
     let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
-
     let message_store = CookieMessageStore::builder(secret_key.clone()).build();
-
     let message_framework = FlashMessagesFramework::builder(message_store).build();
-
     let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
-
     let server = HttpServer::new(move || {
         App::new()
             .wrap(message_framework.clone())
@@ -124,10 +102,14 @@ async fn run(
             ))
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
-            .route("/admin/dashboard", web::get().to(admin_dashboard))
-            .route("/admin/password", web::get().to(change_password_form))
-            .route("/admin/password", web::post().to(change_password))
-            .route("/admin/logout", web::post().to(log_out))
+            .service(
+                web::scope("/admin")
+                    .route("/dashboard", web::get().to(admin_dashboard))
+                    .route("/newsletters", web::post().to(publish_newsletter))
+                    .route("/password", web::get().to(change_password_form))
+                    .route("/password", web::post().to(change_password))
+                    .route("/logout", web::post().to(log_out)),
+            )
             .route("/login", web::get().to(login_form))
             .route("/login", web::post().to(login))
             .route("/health_check", web::get().to(health_check))
@@ -137,13 +119,12 @@ async fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
-            .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
-
     Ok(server)
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct HmacSecret(pub Secret<String>);
