6b4e2e79945c7dd77b7cf79c6a15f47f3b8dd6ce
Ji Park <ji@jipark.dev>
2023-07-02 09:15:27 -0700

    Sync auth code with zer2prod.

--- src/authentication/password.rs
+++ src/authentication/password.rs
@@ -1,10 +1,9 @@
+use crate::telemetry::spawn_blocking_with_tracing;
 use anyhow::Context;
 use argon2::password_hash::SaltString;
 use argon2::{Algorithm, Argon2, Params, PasswordHash, PasswordHasher, PasswordVerifier, Version};
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
-
-use crate::telemetry::spawn_blocking_with_tracing;
 
 #[derive(thiserror::Error, Debug)]
 pub enum AuthError {
@@ -19,13 +18,32 @@ pub struct Credentials {
     pub password: Secret<String>,
 }
 
+#[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
+async fn get_stored_credentials(
+    username: &str,
+    pool: &PgPool,
+) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
+    let row = sqlx::query!(
+        r#"
+        SELECT user_id, password_hash
+        FROM users
+        WHERE username = $1
+        "#,
+        username,
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to performed a query to retrieve stored credentials.")?
+    .map(|row| (row.user_id, Secret::new(row.password_hash)));
+    Ok(row)
+}
+
 #[tracing::instrument(name = "Validate credentials", skip(credentials, pool))]
 pub async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, AuthError> {
     let mut user_id = None;
-
     let mut expected_password_hash = Secret::new(
         "$argon2id$v=19$m=15000,t=2,p=1$\
         gZiV/M1gPc22ElAH/Jh1Hw$\
@@ -52,10 +70,10 @@ pub async fn validate_credentials(
 }
 
 #[tracing::instrument(
-    name = "Verify password hash",
+    name = "Validate credentials",
     skip(expected_password_hash, password_candidate)
 )]
-pub fn verify_password_hash(
+fn verify_password_hash(
     expected_password_hash: Secret<String>,
     password_candidate: Secret<String>,
 ) -> Result<(), AuthError> {
@@ -71,31 +89,6 @@ pub fn verify_password_hash(
         .map_err(AuthError::InvalidCredentials)
 }
 
-#[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
-pub async fn get_stored_credentials(
-    username: &str,
-    pool: &PgPool,
-) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
-    let row = sqlx::query!(
-        r#"
-        SELECT
-            user_id,
-            password_hash
-        FROM
-            users
-        WHERE
-            username = $1;
-        "#,
-        username,
-    )
-    .fetch_optional(pool)
-    .await
-    .context("Failed to perform a query to retrieve stored credentials.")?
-    .map(|row| (row.user_id, Secret::new(row.password_hash)));
-
-    Ok(row)
-}
-
 #[tracing::instrument(name = "Change password", skip(password, pool))]
 pub async fn change_password(
     user_id: uuid::Uuid,
@@ -104,14 +97,12 @@ pub async fn change_password(
 ) -> Result<(), anyhow::Error> {
     let password_hash = spawn_blocking_with_tracing(move || compute_password_hash(password))
         .await?
-        .context("Failed to hash password.")?;
+        .context("Failed to hash password")?;
     sqlx::query!(
         r#"
         UPDATE users
-        SET
-            password_hash = $1
-        WHERE
-            user_id = $2
+        SET password_hash = $1
+        WHERE user_id = $2
         "#,
         password_hash.expose_secret(),
         user_id
